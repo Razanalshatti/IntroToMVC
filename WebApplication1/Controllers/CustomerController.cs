13d4e1371646c787abcaa163740c5a4c4ca4d1e9
KFH <KFH@RAZAN-ALSHATTI>
2024-04-16 13:53:37 +0300

    Add project files.

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(CustomerData.Customers);
+        }
+        public IActionResult Details(int id)
+        {
+            var customer = CustomerData.Customers[id];
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+    }
+}
---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public int phone { get; set; }
+        public string country { get; set; }
+    }
+
+    public static class CustomerData
+    {
+        public static List<Customer> Customers = new List<Customer>
+        {
+            new Customer { id = 1, name = "Razan Alshatti" , email = "razan@gmail.com" , phone = 99887766 , country = "kuwait" },
+            new Customer { id = 2, name = "Nada Alshaibani" , email = "nada@gmail.com" , phone = 66778899 , country = "kuwait" }
+        };
+    }
+}
