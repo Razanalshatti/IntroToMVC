cc747fcc185077d5011799003b73eed8530e6df5
Razan Alshatti <83327@kfh.com>
2024-04-17 11:49:51 +0300

    editing onvalidation model

--- WebApplication1/Models/AddCustomerForm.cs
+++ WebApplication1/Models/AddCustomerForm.cs
@@ -5,13 +5,23 @@ namespace WebApplication1.Models
     public class AddCustomerForm
     {
         [Required]
-        public string name {  get; set; }
+        [Display(Name = "Customer Name")]
+        public string name { get; set; }
+
         [EmailAddress]
+        [Display(Name = "Customer Email")]
         public string email { get; set; }
-        [RegularExpression("^([569]\\d{7})$")]
+
+        [RegularExpression("^([569]\\d{7})$", ErrorMessage ="Invalid phone number. please enter an kuwait phone number")]
+        [Display(Name = "Customer Phone number")]
         public string phone { get; set; }
+
         [Required]
+        [Display(Name = "Customer Country")]
         public string country { get; set; }
 
+        //[DataType(DataType.Date)]
+        //[Display(Name = "Date of Birth")]
+        //public DateOnly Dob { get; set; }
     }
 }
